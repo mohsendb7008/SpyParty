3d18c13ea683b0c8454ad3efc4fcfb78de33e09e
Mohsen Dehbashi <mohsendb7008@gmail.com>
2025-09-05 00:28:27 +0200

    implemented confirm role in game phase

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 const gameState = {
     word: null,
+    confirmed: new Set(),
     isSpy() {
         return this.word == null;
+    },
+    allConfirmed() {
+        return this.confirmed.size === lobbyState.players.length;
     }
 }
 
@@ -10,11 +14,27 @@ function handleGameMessage(msg) {
         case 'start':
         {
             appState.phase = 'game';
+            gameState.word = null;
             if (!msg.spies.some(spy => spy.id === lobbyState.playerId)) {
                 gameState.word = msg.word;
+            }
+            gameState.confirmed.clear();
+            return true;
+        }
+        case 'confirm':
+        {
+            gameState.confirmed.add(msg.id);
+            if (gameState.allConfirmed()) {
+                appState.phase = 'timer';
             }
             return true;
         }
     }
     return false;
 }
+
+$('confirmBtn').onclick = () => {
+    if (gameState.confirmed.has(lobbyState.playerId)) return;
+    send({ type: 'confirm', code: lobbyState.gameCode, id: lobbyState.playerId });
+    render();
+};
